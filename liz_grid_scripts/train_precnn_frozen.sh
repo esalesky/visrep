651f2ee5d8b6c67ca61bcf780cbff93352e69bfe
Elizabeth Salesky <elizabeth.salesky@gmail.com>
2019-11-19 15:50:39 -0500

    - checking in liz baseline grid scripts

---
+++ liz_grid_scripts/train_precnn_frozen.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#. /etc/profile.d/modules.sh
+#
+#$ -S /bin/bash -q gpu.q@@2080 -cwd 
+#$ -l h_rt=48:00:00,gpu=1 
+#$ -N preconv_frozen
+# num_proc=16,mem_free=32G,
+
+# Train Transformer model
+# -----------------------
+
+module load cuda10.0/toolkit/10.0.130
+module load cudnn/7.5.0_cuda10.0
+
+if [ ! -z $SGE_HGR_gpu ]; then
+    export CUDA_VISIBLE_DEVICES=$SGE_HGR_gpu
+    sleep 3
+fi
+
+source activate /home/hltcoe/esalesky/anaconda3/envs/fs
+
+export LD_LIBRARY_PATH=/cm/local/apps/gcc/7.2.0/lib64:$LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+echo $PYTHONPATH
+echo $CUDA_VISIBLE_DEVICES
+nvidia-smi
+hostname
+
+
+SRC_LANG=$1
+TGT_LANG=en
+FAIRSEQ_PATH=/exp/esalesky/mtocr19/fairseq
+DATA_DIR=$2  
+CKPT_DIR=$3
+SEGM=$4  
+
+echo $DATA_DIR
+echo $FAIRSEQ_PATH
+echo $SRC_LANG
+echo $TGT_LANG
+echo $CKPT_DIR
+
+mkdir -p $CKPT_DIR
+
+python $FAIRSEQ_PATH/train.py \
+$DATA_DIR \
+--source-lang=$SRC_LANG \
+--target-lang=$TGT_LANG \
+--user-dir=$FAIRSEQ_PATH \
+--arch=transformer_iwslt_de_en \
+--share-decoder-input-output-embed \
+--encoder-embed-path=/expscratch/detter/mt/multitarget-ted/visemb/$SRC_LANG-$TGT_LANG/$SEGM/norm_word_embeddings.txt \
+--freeze-encoder-embed \
+--optimizer=adam \
+--adam-betas='(0.9, 0.98)' \
+--clip-norm=0.0 \
+--lr=5e-4 \
+--lr-scheduler=inverse_sqrt \
+--warmup-updates=4000 \
+--dropout=0.3 \
+--weight-decay=0.0001 \
+--criterion=label_smoothed_cross_entropy \
+--label-smoothing=0.3 \
+--max-epoch=100 \
+--num-workers=0 \
+--save-dir=$CKPT_DIR \
+--raw-text \
+--no-epoch-checkpoints \
+--log-format=simple \
+--max-tokens=4000 \
+--update-freq=8 \
+--log-interval=10 2>&1 | tee $CKPT_DIR/train.log
+
+# only store last and best checkpoints
