04874ef53b002199e74fb65e445030d018bedcac
Liz Salesky <liz_salesky@apple.com>
2023-08-22 10:06:06 -0700

    nits

--- fairseq/data/visual/__init__.py
+++ fairseq/data/visual/__init__.py
@@ -1,9 +1,9 @@
 from .image_generator import TextImageGenerator
 from .visual_text_dataset import VisualTextDataset
-from .visual_text_dataset_augmentation import VisualTextDatasetAug
+from .visual_text_dataset_augmentation import VisualTextAugDataset
 
 __all__ = [
     "TextImageGenerator",
     "VisualTextDataset",
-    "VisualTextDatasetAug",
+    "VisualTextAugDataset",
 ]
